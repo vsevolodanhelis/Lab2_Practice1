a89eeaa11bf3f02146bf56288614e2dab2211ca9
Małgorzata Jagieła <malgorzata.jagiela@hotmail.com>
2025-03-18 15:04:53 +0100

    Small fix

--- Geometry/Geometry/Segment.cs
+++ Geometry/Geometry/Segment.cs
@@ -2,15 +2,6 @@
 //
 // public class Segment(Point start, Point end)
 // {
-//     public override bool Equals(object? obj)
-//     {
-//         var other = obj as Segment;
-//         return Start.Equals(other.Start) && End.Equals(other.End);
-//     }
-//
-//     public Point Start { get; } = start;
-//
-//     public Point End { get; } = end;
 //     //Your code here
 //     
 // }
